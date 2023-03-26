658ae4c5d9da56eb39b884a8453ed48a8fb338a6
Samitha Bandara <122595628+samitha278@users.noreply.github.com>
2023-03-26 15:02:32 +0530

    Update problem #14 (2).py
    no errors

--- problem #14 (2).py
+++ problem #14 (2).py
@@ -1,49 +1,48 @@
 class Solution:
     def collatz(self, n):
-        longest_chain = 1
-        max_items = 1
         
         collatz_dic = {1:1}
         
         for i in range(2, n+1):
             
-            chain_items = 0
-            p = i
-            
-            collatz_sub_dic = {}
-            
-            while p != 1:
+            if i in collatz_dic:
+                continue
+            else:    
+                chain_items = 0
+                p = i
+                collatz_sub_dic = {}
                 
-                if p not in collatz_dic:
-                    collatz_sub_dic[p] = chain_items 
-            
-                    if p % 2 == 0:
-                        p = p // 2
-                    else:
-                        p = 3 * p + 1
-                        
-                if p in collatz_dic:
-                    chain_items += collatz_dic[p]+1
-                    break
+                while p != 1:
+                    
+                    if p not in collatz_dic:
+                        collatz_sub_dic[p] = chain_items 
                 
-                chain_items += 1
-                
-            if collatz_sub_dic != {}:
+                        if p % 2 == 0:
+                            p = p // 2
+                        else:
+                            p = 3 * p + 1
+                            
+                    if p in collatz_dic:
+                        chain_items += collatz_dic[p]+1
+                        break
+                    
+                    chain_items += 1
+                    
                 for j in collatz_sub_dic:
                     collatz_sub_dic[j] = chain_items - collatz_sub_dic[j]
-                
+                    
                 collatz_dic.update(collatz_sub_dic)
-
-            if max_items < chain_items:
-                max_items = chain_items
-                longest_chain = i
+                
+                collatz_sub_dic.clear()
+                
+        key_max = max(collatz_dic, key= lambda x: collatz_dic[x])
         
-        return longest_chain,max_items
+        return {"longest_chain":key_max , "chain_items":collatz_dic[key_max] }
 
 def main():
     x = Solution()
-    y = x.collatz(100)
-    print("number:",y[0],"\nitems in chain:",y[1])
+    y = x.collatz(1000000)
+    print("number:",y["longest_chain"],"\nitems in chain:",y["chain_items"])
 
 if __name__ == '__main__':
     main()
