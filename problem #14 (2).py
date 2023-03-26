527311101d6af3f9fe115ff1ac8246a91de6ce8f
Samitha Bandara <122595628+samitha278@users.noreply.github.com>
2023-03-26 14:34:26 +0530

    Update problem #14 (2).py
    not finish yet

--- problem #14 (2).py
+++ problem #14 (2).py
@@ -6,7 +6,8 @@ class Solution:
         collatz_dic = {1:1}
         
         for i in range(2, n+1):
-            chain_items = 1
+            
+            chain_items = 0
             p = i
             
             collatz_sub_dic = {}
@@ -14,23 +15,24 @@ class Solution:
             while p != 1:
                 
                 if p not in collatz_dic:
-                    collatz_sub_dic[p] = chain_items
+                    collatz_sub_dic[p] = chain_items 
             
                     if p % 2 == 0:
                         p = p // 2
                     else:
                         p = 3 * p + 1
                         
-                else:
-                    chain_items += collatz_dic[p]
+                if p in collatz_dic:
+                    chain_items += collatz_dic[p]+1
                     break
                 
                 chain_items += 1
                 
-            for j in collatz_sub_dic:
-                collatz_sub_dic[j] = chain_items - collatz_sub_dic[j] + 1
+            if collatz_sub_dic != {}:
+                for j in collatz_sub_dic:
+                    collatz_sub_dic[j] = chain_items - collatz_sub_dic[j]
                 
-            collatz_dic.update(collatz_sub_dic)
+                collatz_dic.update(collatz_sub_dic)
 
             if max_items < chain_items:
                 max_items = chain_items
@@ -40,7 +42,7 @@ class Solution:
 
 def main():
     x = Solution()
-    y = x.collatz(1000000)
+    y = x.collatz(100)
     print("number:",y[0],"\nitems in chain:",y[1])
 
 if __name__ == '__main__':
